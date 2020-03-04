28d9f21111ddafef4d454320e2151df2a19eb9a4
rasha <angeldream249@yahoo.com>
2020-03-04 04:24:44 -0800

    Done.

--- src/data.js
+++ src/data.js
@@ -1,17 +1,20 @@
 const movieData = [
   {
+    id:1,
     title: 'Choux and Maru go to Istanbul',
     IMDBRating: 3,
     genres: ['cats', 'adventure', 'romance'],
     poster: 'choux-maru-istanbul'
   },
   {
+    id:2,
     title: 'Choux and Maru',
     IMDBRating: 4,
     genres: ['cats', 'drama', 'romance'],
     poster: 'choux-maru-part-1'
   },
   {
+    id:3,
     title: 'ChromeBoi',
     IMDBRating: 5,
     genres: ['thriller', 'romance'],
@@ -19,36 +22,42 @@ const movieData = [
   },
   
   {
+    id:4,
     title: 'Escape from VIM',
     IMDBRating: 0,
     genres: ['an awful movie'],
     poster: 'escape-from-vim'
   },
   {
+    id:5,
     title: 'GoldenEye',
     IMDBRating: 4,
     genres: ['mystery', 'thriller', 'crime'],
     poster: 'goldeneye'
   },
   {
+    id:6,
     title: 'Handsome Boy Modeling Club',
     IMDBRating: 2,
     genres: ['coming of age', 'romance'],
     poster: 'handsome-boy'
   },
   {
+    id:7,
     title: "Maru's Spinoff: Trapped in the Sheets",
     IMDBRating: 5,
     genres: ['Adventure'],
     poster: 'marus-spinoff'
   },
   {
+    id:8,
     title: 'Terrance: King of the Rats',
     IMDBRating: 1,
     genres: ['drama'],
     poster: 'terrance-king'
   },
   {
+    id:9,
     title: 'The Trash Man',
     IMDBRating: 5,
     genres: ['crime', 'period piece'],
