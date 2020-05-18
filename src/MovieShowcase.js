818e754f034f35f69714131f2606bd02b51d9a65
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-05-18 18:20:44 +0300

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,9 +6,17 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movie, index) => {
+      return <MovieCard 
+                title={movie.title}
+                IMDBRating={movie.IMDBRating}
+                genres={movie.genres}
+                poster={movie.poster}
+              />  
+    });
   }
 
-  render() {
+render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
